b5141608a86a3b9d92811e29d197afa6f8098da0
Mohamed <mohamedhelmoghazy.gmail.com>
2020-08-25 21:21:58 -0600

    Starting Respository with Interfaces

---
+++ PurchaseRequestApproval.DataAccess/Repository/IRepository/IRepository.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+
+namespace PurchaseRequestApproval.DataAccess.Repository.IRepository
+{
+   public  interface IRepository<T> where T: class // Because it is generic interface repository 
+    {
+        T Get(int Id); // get the class with the matched ID
+
+        // to get a list of items with specific category 
+        IEnumerable<T> GetAll(
+    Expression<Func<T, bool>> filter = null,
+    Func<IQueryable<T>, IOrderedQueryable<T>> orderBy = null,
+    string includeProperties = null
+    );
+
+        // to get a list of items header 
+        T GetFirstOrDefault(
+            Expression<Func<T, bool>> filter = null, 
+            string includeProperties = null
+            );
+
+        // method to add an entity
+        void Add(T entity);
+
+        // method to remove any entity with ID
+        void Remove(int id);
+     
+        // to remvoe entity with specic properties 
+        void Remove(T entity);
+
+        // to remvoe entity range of entities 
+        void RemoveRange(IEnumerable <T> entity);
+
+
+
+
+
+    }
+}
---
+++ PurchaseRequestApproval.DataAccess/Repository/ShippingRepository.cs
@@ -0,0 +1,30 @@
+﻿using PurchaseRequestApproval.DataAccess.Data;
+using PurchaseRequestApproval.DataAccess.Repository.IRepository;
+using PurchaseRequestApproval.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace PurchaseRequestApproval.DataAccess.Repository
+{
+    public class ShippingRepository : Repository<Shipping>, IShippingRepository
+    {
+        private readonly ApplicationDbContext _db;
+        public ShippingRepository(ApplicationDbContext db) : base(db)
+        {
+            _db = db;
+
+        }
+
+        public void Update(Shipping shipping)
+        {
+            var objFromDb = _db.Shippings.FirstOrDefault(s=>s.Id==shipping.Id);
+            if(objFromDb != null )
+            { 
+             objFromDb.ShippingName = shipping.ShippingName;
+             _db.SaveChanges();
+            }
+        }
+    }
+}
