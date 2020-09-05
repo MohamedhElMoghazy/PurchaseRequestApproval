3f54414a0755fc6ab6c06a0d025d42d07117b2be
Mohamed <mohamedhelmoghazy.gmail.com>
2020-09-05 14:01:04 -0600

    Arrange Store Procedure for Shipping

--- PurchaseRequestApproval/Areas/Admin/Controllers/ShippingController.cs
+++ PurchaseRequestApproval/Areas/Admin/Controllers/ShippingController.cs
@@ -3,10 +3,12 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
 using System.Threading.Tasks;
+using Dapper;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.VisualStudio.Web.CodeGeneration.Contracts.Messaging;
 using PurchaseRequestApproval.DataAccess.Repository.IRepository;
 using PurchaseRequestApproval.Models;
+using PurchaseRequestApproval.Utility;
 
 namespace PurchaseRequestApproval.Areas.Admin.Controllers
 {
@@ -34,7 +36,15 @@ namespace PurchaseRequestApproval.Areas.Admin.Controllers
 
             }
             // this for edit request
-            shipping = _unitOfWork.Shipping.Get(id.GetValueOrDefault());
+            //shipping = _unitOfWork.Shipping.Get(id.GetValueOrDefault());
+            var parameter = new DynamicParameters(); // arrange parameters for sql server
+            parameter.Add("@Id", id); // arrange to send the Id
+
+
+            // var objFromDb = _unitOfWork.Shipping.Get(id);
+
+            shipping= _unitOfWork.SP_Call.OneRecord<Shipping>(SD.Proc_Shipping_Get, parameter);
+
             if (shipping == null) 
             { return NotFound(); }
             return View(shipping);
@@ -48,16 +58,26 @@ namespace PurchaseRequestApproval.Areas.Admin.Controllers
         { 
             if (ModelState.IsValid)
             {
+                // to pass parameters to sql procedrues
+                var parameter = new DynamicParameters();
+                parameter.Add("@ShippingName", shipping.ShippingName);
+                parameter.Add("@ShippingDescription", shipping.ShippingDescription);
+                parameter.Add("@ShippingACC", shipping.ShippingACC);
+                
+
                 if (shipping.Id==0) // create case whenever no ID posted
                 {
-                    _unitOfWork.Shipping.Add(shipping);
-                    
+                    //_unitOfWork.Shipping.Add(shipping);
+                    _unitOfWork.SP_Call.Execute(SD.Proc_Shipping_Create, parameter);
+
 
                 }
                 else
                 {
-                    _unitOfWork.Shipping.Update(shipping);
-                    
+                    //_unitOfWork.Shipping.Update(shipping);
+                    parameter.Add("@Id", shipping.Id);
+                    _unitOfWork.SP_Call.Execute(SD.Proc_Shipping_Update, parameter);
+
                 }
                 _unitOfWork.Save();
                 return RedirectToAction(nameof(Index)); // if any mistake the name is gotted
@@ -76,29 +96,33 @@ namespace PurchaseRequestApproval.Areas.Admin.Controllers
         public IActionResult GetAll()
          {
 
-            var allObj = _unitOfWork.Shipping.GetAll();
+            // var allObj = _unitOfWork.Shipping.GetAll();
+            var allObj = _unitOfWork.SP_Call.List<Shipping>(SD.Proc_Shipping_GetAll, null); // to allow stored procedure
             return Json(new { data = allObj });
 
 
-            /*
-                    public async Task<IActionResult> GetAll()
-                        {
-                            var allObj = await _unitOfWork.Shipping.GetAllAsync();
-                            return Json(new { data = allObj });
-                        }
-            */
+            
 
 
         }
         [HttpDelete]
         public IActionResult Delete(int id)
         {
-            var objFromDb = _unitOfWork.Shipping.Get(id);
+            var parameter = new DynamicParameters(); // arrange parameters for sql server
+            parameter.Add("@Id", id); // arrange to send the Id
+
+
+            // var objFromDb = _unitOfWork.Shipping.Get(id);
+
+            var objFromDb = _unitOfWork.SP_Call.OneRecord<Shipping>(SD.Proc_Shipping_Get, parameter);
+
             if (objFromDb == null)
             {
                 return Json(new { success = false, Message = "Error while deleting" });
             }
-            _unitOfWork.Shipping.Remove(objFromDb);
+            //_unitOfWork.Shipping.Remove(objFromDb);
+            _unitOfWork.SP_Call.Execute(SD.Proc_Shipping_Delete, parameter); // Pass parameters to execute sql procedures
+
             _unitOfWork.Save(); 
             return Json(new { success = true, Message = "Delete Successful" });
 
