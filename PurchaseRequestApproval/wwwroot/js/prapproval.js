397579d25fe12d48957341d0028e6cc50eafdc20
Mohamed <mohamedhelmoghazy.gmail.com>
2020-09-08 21:57:48 -0600

    Start Views for Quote and Vendor Contact without Upsert

---
+++ PurchaseRequestApproval/wwwroot/js/prapproval.js
@@ -0,0 +1,68 @@
+﻿var dataTable;
+ 
+$(document).ready(function () {
+    loadDataTable();
+});
+
+
+    function loadDataTable() {
+        dataTable = $('#tblData').DataTable({
+            "ajax": {
+                "url": "/Admin/PRApproval/GetAll"
+            },
+
+            "columns": [
+
+                { "data": "employeeName", "width ": "15%" },
+                { "data": "employeeCode", "width ": "15%" },
+                { "data": "employeePosition", "width ": "15%" },
+                { "data": "employeePhone", "width ": "15%" },
+                { "data": "employeeEmail", "width ": "15%" },
+                { "data": "employeeSite", "width ": "15%" },
+
+
+
+
+                {
+                    "data": "id",
+                    "render": function (data) {
+                        return `
+                            <div class="text-center">
+                                <a href="/Admin/PRApproval/Upsert/${data}" class="btn btn-success text-white" style="cursor:pointer">
+                                    <i class="fas fa-edit"></i> 
+                                </a>
+                                <a onclick=Delete("/Admin/PRApproval/Delete/${data}") class="btn btn-danger text-white" style="cursor:pointer">
+                                    <i class="fas fa-trash-alt"></i> 
+                                </a>
+                            </div>
+                           `;
+                    }, "width": "10%"
+                }
+            ]
+        });
+}
+function Delete(url) {
+    swal({
+        title: "Are you sure you want to Delete?",
+        text: "You will not be able to restore the data!",
+        icon: "warning",
+        buttons: true,
+        dangerMode: true
+    }).then((willDelete) => {
+        if (willDelete) {
+            $.ajax({
+                type: "DELETE",
+                url: url,
+                success: function (data) {
+                    if (data.success) {
+                        toastr.success(data.message);
+                        dataTable.ajax.reload();
+                    }
+                    else {
+                        toastr.error(data.message);
+                    }
+                }
+            });
+        }
+    });
+}
---
+++ PurchaseRequestApproval/wwwroot/js/vendorcontact.js
@@ -0,0 +1,68 @@
+﻿var dataTable;
+ 
+$(document).ready(function () {
+    loadDataTable();
+});
+
+
+    function loadDataTable() {
+        dataTable = $('#tblData').DataTable({
+            "ajax": {
+                "url": "/Admin/VendorContact/GetAll"
+            },
+
+            "columns": [
+
+                { "data": "name", "width ": "15%" },
+                { "data": "email", "width ": "15%" },
+                { "data": "phone", "width ": "15%" },
+                { "data": "branch", "width ": "15%" },
+                { "data": "remark", "width ": "15%" },
+                { "data": "vendor.VendorName", "width ": "15%" },
+
+
+
+
+                {
+                    "data": "id",
+                    "render": function (data) {
+                        return `
+                            <div class="text-center">
+                                <a href="/Admin/VendorContact/Upsert/${data}" class="btn btn-success text-white" style="cursor:pointer">
+                                    <i class="fas fa-edit"></i> 
+                                </a>
+                                <a onclick=Delete("/Admin/VendorContact/Delete/${data}") class="btn btn-danger text-white" style="cursor:pointer">
+                                    <i class="fas fa-trash-alt"></i> 
+                                </a>
+                            </div>
+                           `;
+                    }, "width": "10%"
+                }
+            ]
+        });
+}
+function Delete(url) {
+    swal({
+        title: "Are you sure you want to Delete?",
+        text: "You will not be able to restore the data!",
+        icon: "warning",
+        buttons: true,
+        dangerMode: true
+    }).then((willDelete) => {
+        if (willDelete) {
+            $.ajax({
+                type: "DELETE",
+                url: url,
+                success: function (data) {
+                    if (data.success) {
+                        toastr.success(data.message);
+                        dataTable.ajax.reload();
+                    }
+                    else {
+                        toastr.error(data.message);
+                    }
+                }
+            });
+        }
+    });
+}
